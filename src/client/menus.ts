562833fae9868439bb402dba50cc5cd4dc6f9e36
ansiboy <ansiboy@163.com>
2017-03-11 09:15:24 +0800

    重构菜单

--- src/client/menus.ts
+++ src/client/menus.ts
@@ -1,14 +1,41 @@
-let menus = [
-    {
-        "title": "用户列表",
-        "icon": "icon-bullhorn",
-         "url": "#home/users"
-    },
-    {
-        "title": "URL拦截",
-        "icon": "icon-bullhorn",
-         "url": "#permission/urlSetting"
-    }
-];
+import * as app from 'application';
 
-export = menus;
+export interface MenuItem {
+    name: string,
+    title: string,
+    icon: string,
+    url: string,
+    children: MenuItem[]
+}
+
+
+export default function () {
+    let appId = app.currentPage.routeData.values.appId;
+    let menus: MenuItem[] = [
+        {
+            name: 'home.users',
+            title: "用户列表",
+            icon: "icon-bullhorn",
+            url: `home/users?appId=${appId}`
+        } as MenuItem,
+        {
+            name: 'urlSetting',
+            title: 'URL设置',
+            icon: 'icon-bullhorn',
+            children: [
+                {
+                    name: 'permission.urlSetting',
+                    title: "URL拦截",
+                    url: `permission/urlSetting?appId=${appId}`
+                } as MenuItem,
+                {
+                    name: 'home.redirect',
+                    title: 'URL转发',
+                    url: `home/redirect?appId=${appId}`
+                } as MenuItem
+            ]
+        } as MenuItem
+    ];
+
+    return menus;
+};
